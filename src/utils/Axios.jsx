16c02ff89affe28d1648ada07ecf9c8e32cb22ff
Chandramouli989 <chandramoulipatle@gmail.com>
2026-08-15 01:37:44 +0530

    Add TMDB API proxy

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-// Simple axios instance for TMDB API
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  timeout: 30000, // 30 second timeout for slow networks
+  baseURL: "/api/tmdb",
+  timeout: 30000,
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWRmMmEwNGZkYjRjYjFlOWExMjliZjkyNzE0MThlZiIsIm5iZiI6MTc0ODA3MTE0OC4wMDYsInN1YiI6IjY4MzE3MmVjMDg4OWQzYjRiNzQxMzhiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L-AQEQTG3UqmmNG4Ga2KC4ANoInHeQaAmQzXjbmyAnM'
-  }
+    accept: "application/json",
+  },
 });
 
 export default instance;
